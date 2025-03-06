45a825f8ded0fab53c920f07628b2fbe729efbe1
EntrepaDeFuet <orioljb1999@gmail.com>
2025-03-06 18:24:05 +0100

    Canvis 06/03/25: 1) Arreglats els constructors de les classes. 2)Renombrament de funcions 3)Tests funcionen més o menys tots ok! 4) Reserva llança excepcions.

--- P2-2425-Practica1/CodiBase/model/Reserva.java
+++ P2-2425-Practica1/CodiBase/model/Reserva.java
@@ -1,5 +1,7 @@
 package prog2.vista.model;
+import prog2.vista.vista.ExcepcioReserva;
 
+import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
 
 public class Reserva {
@@ -8,12 +10,15 @@ public class Reserva {
     private Client client;
     private Allotjament allotjament;
 
-    public Reserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida){
-
-        setAllotjament_(allotjament);
-        setClient(client);
-        setDataEntrada(dataEntrada);
-        setDataSortida(dataSortida);
+    public Reserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva{
+        if(isReserva(allotjament,dataEntrada,dataSortida)) {
+            setAllotjament_(allotjament);
+            setClient(client);
+            setDataEntrada(dataEntrada);
+            setDataSortida(dataSortida);
+        } else {
+            throw new ExcepcioReserva("Aquesta reserva no compleix l'estada mínima.");
+        }
 
     }
 
@@ -47,5 +52,43 @@ public class Reserva {
     //Altres:
     public String toString(){
         return "Allotjament reservat: " +this.allotjament.toString()+ "\n Client que ha fet la reserva: " + this.client.toString() + "\n Data d'entrada: "+ this.getDataEntrada().toString() + " Data de sortida:"+this.getDataSortida();
+    }
+    public static InAllotjament.Temp getTemporada_(LocalDate data){
+
+        LocalDate tempAltaInici = LocalDate.of(2025, 3, 21);
+        LocalDate tempAltaFinal = LocalDate.of(2025, 9, 20);
+
+        //Aquestes variables les hem fet per llegibilitat de codi.
+        boolean condicio1, condicio2;
+
+        condicio1 = data.equals(tempAltaInici)|| data.isAfter(tempAltaInici);
+        condicio2 = data.equals(tempAltaFinal) || data.isBefore(tempAltaFinal);
+
+        if(condicio1 && condicio2){
+
+            return InAllotjament.Temp.ALTA;
+
+        } else{
+
+            return InAllotjament.Temp.BAIXA;
+        }
+
+    }
+    public boolean isReserva(Allotjament allotjament,LocalDate dataEntrada,LocalDate dataSortida){
+
+        long estadaMinima_,estada;
+        estada = ChronoUnit.DAYS.between(dataEntrada, dataSortida);
+
+        return switch (getTemporada_(dataEntrada)) {
+            case ALTA -> {
+                estadaMinima_ = allotjament.getEstadaMinima(InAllotjament.Temp.ALTA);
+                yield estada >= estadaMinima_;
+            }
+            case BAIXA -> {
+                estadaMinima_ = allotjament.getEstadaMinima(InAllotjament.Temp.BAIXA);
+                yield estada >= estadaMinima_;
+            }
+            default -> false;
+        };
     }
 }
